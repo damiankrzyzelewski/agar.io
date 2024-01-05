272def9fd4d77b90a71139f1247a42d500096175
damiankrzyzelewski <krzyzelewskid@gmail.com>
2024-01-05 13:58:32 +0100

    Dodanie generacji małych kulek, jedzenia ich przez graczy i dostosowywania prędkości do rozmiaru

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from network import Network
 
+
 class Canvas:
 
     def __init__(self, w, h, name="None"):
@@ -29,35 +30,40 @@ class Canvas:
 class Player:
     radius = 25  # Set the radius for the circles
 
-    def __init__(self, startx, starty, color=(255, 0, 0)):
+    def __init__(self, startx, starty, max_x, min_x, max_y, min_y, color=(255, 0, 0)):
         self.x = startx
         self.y = starty
-        self.velocity = 2
+        self.velocity = int(50 / 10**(5/6))
         self.color = color
+        self.max_x = max_x
+        self.min_x = min_x
+        self.max_y = max_y
+        self.min_y = min_y
 
     def draw(self, g):
         pygame.draw.circle(g, self.color, (self.x + self.radius, self.y + self.radius), self.radius)
 
     def move(self, dirn):
-        if dirn == 0:
+        if dirn == 0 and self.x + self.velocity + 2*self.radius <= self.max_x:
             self.x += self.velocity
-        elif dirn == 1:
+        elif dirn == 1 and self.x + self.radius - self.velocity >= self.min_x:
             self.x -= self.velocity
-        elif dirn == 2:
+        elif dirn == 2 and self.y + self.radius - self.velocity >= self.min_y:
             self.y -= self.velocity
-        else:
+        elif dirn == 3 and self.y + self.velocity + 2*self.radius <= self.max_y:
             self.y += self.velocity
 
 
 class Game:
 
     def __init__(self, w, h):
+        self.small_balls = None
         self.net = Network()
         self.width = w
         self.height = h
-        self.player = Player(50, 50)
-        self.player2 = Player(50, 50)
-        self.player3 = Player(50, 50)
+        self.player = Player(50, 50, self.width, 0, self.height, 0)
+        self.player2 = Player(50, 50, self.width, 0, self.height, 0)
+        self.player3 = Player(50, 50, self.width, 0, self.height, 0)
         self.canvas = Canvas(self.width, self.height, "Testing...")
 
     def run(self):
@@ -70,34 +76,36 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-
-                if event.type == pygame.K_ESCAPE:
-                    run = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        run = False
 
             keys = pygame.key.get_pressed()
 
             if keys[pygame.K_RIGHT]:
                 if self.player.x <= self.width - self.player.velocity:
                     self.player.move(0)
-
             if keys[pygame.K_LEFT]:
                 if self.player.x >= self.player.velocity:
                     self.player.move(1)
-
             if keys[pygame.K_UP]:
                 if self.player.y >= self.player.velocity:
                     self.player.move(2)
-
             if keys[pygame.K_DOWN]:
                 if self.player.y <= self.height - self.player.velocity:
                     self.player.move(3)
-            if not initial_positions_received:
-                initial_positions_received = self.receive_initial_positions()
-            # Send Network Stuff
-            self.player2.x, self.player2.y, self.player3.x, self.player3.y = self.parse_data(self.send_data(), self.net.id)
 
+            if not initial_positions_received:
+                initial_positions_received, small_balls_info = self.receive_initial_positions()
+                self.initialize_small_balls(small_balls_info)
+
+            # Send Network Stuff
+            self.player.radius, self.player2.x, self.player2.y, self.player2.radius, self.player3.x, self.player3.y, self.player3.radius, self.small_balls = self.parse_data(
+                self.send_data(), self.net.id)
+            self.player.velocity = int(50 / self.player.radius**(5/6))
             # Update Canvas
             self.canvas.draw_background()
+            self.draw_small_balls()
             self.player.draw(self.canvas.get_canvas())
             self.player2.draw(self.canvas.get_canvas())
             self.player3.draw(self.canvas.get_canvas())
@@ -107,42 +115,80 @@ class Game:
 
     def receive_initial_positions(self):
         initial_positions = self.net.receive()
-        if initial_positions and ":" in initial_positions:
-            positions = initial_positions.split("|")
-            if len(positions) == 3:
-                for player_info in positions:
-                    player_id, position = player_info.split(":")
+        print(initial_positions)
+        if initial_positions and "?" in initial_positions:
+            player_positions, small_balls_info = initial_positions.split("?")
+            player_positions = player_positions.split("|")
+            if len(player_positions) == 3:
+                for player_info in player_positions:
+                    player_id, player_data = player_info.split(":")
+                    x, y, radius = map(int, player_data.split(","))
+
                     if int(player_id) == int(self.net.id):
-                        x, y = map(int, position.split(","))
                         self.player.x = x
                         self.player.y = y
-                        return True
-        return False
+                        self.player.radius = radius
+                        return True, small_balls_info
+        return False, ""
 
+    def draw_small_balls(self):
+        for ball in self.small_balls:
+            pygame.draw.circle(self.canvas.get_canvas(), (0, 0, 255), (ball['x'], ball['y']), 5)
+
+    def initialize_small_balls(self, small_balls_info):
+        self.small_balls = []
+        balls_data = small_balls_info.split("|")
+        for ball_data in balls_data:
+            x, y = map(int, ball_data.split(","))
+            self.small_balls.append({'x': x, 'y': y})
+            
     def send_data(self):
-        data = str(self.net.id) + ":" + str(self.player.x) + "," + str(self.player.y)
+        data = f"{self.net.id}:{self.player.x},{self.player.y},{self.player.radius}"
         reply = self.net.send(data)
         return reply
 
     @staticmethod
     def parse_data(data, current_player_id):
         try:
-            player_positions = []
-            for player_info in data.split("|"):
-                player_id, position = player_info.split(":")
-                x, y = map(int, position.split(","))
-                player_positions.append((x, y))
+            player_info_list = []
+            current_player_radius = 0  # Zmienna do przechowywania radiusa aktualnego gracza
+
+            # Rozdzielamy dane dotyczące graczy i małych piłek
+            players_and_balls = data.split("?")
+            players_data = players_and_balls[0]
+            balls_data = players_and_balls[1]
+
+            # Parsujemy dane graczy
+            for player_info in players_data.split("|"):
+                player_id, player_data = player_info.split(":")
+                x, y, radius = map(int, player_data.split(","))
+                player_info_list.append((x, y, radius))
+
+                # Jeśli to aktualny gracz, zapisz jego radius
+                if int(player_id) == int(current_player_id):
+                    current_player_radius = radius
 
             # Pobieramy indeks bieżącego gracza
             current_player_index = int(current_player_id)
 
             # Usuwamy dane bieżącego gracza z listy
-            player_positions.pop(current_player_index)
+            player_info_list.pop(current_player_index)
 
-            # Zwracamy pozycję bieżącego gracza i pozycje obu przeciwników jako osobne wartości
-            return player_positions[0][0], player_positions[0][1], player_positions[1][0], player_positions[1][1]
-        except:
-            return 0, 0, 0, 0
+            # Parsujemy dane małych piłek
+            balls_list = []
+            for ball_info in balls_data.split("|"):
+                ball_x, ball_y = map(int, ball_info.split(","))
+                balls_list.append({'x': ball_x, 'y': ball_y})
+
+            # Zwracamy pozycję bieżącego gracza, pozycje obu przeciwników, radius aktualnego gracza i pozycje małych piłek jako osobne wartości
+            return (
+                current_player_radius, player_info_list[0][0], player_info_list[0][1], player_info_list[0][2],
+                player_info_list[1][0], player_info_list[1][1], player_info_list[1][2],
+                balls_list
+            )
+        except Exception as e:
+            print(f"Error parsing data: {e}")
+            return 0, 0, 0, 0, 0, 0, 0, []
 
 
 if __name__ == "__main__":
--- server.cpp
+++ server.cpp
@@ -4,12 +4,62 @@
 #include <cstring>
 #include <string>
 #include <cstdlib>
+#include <cmath>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <mutex>
 
-std::vector<std::string> pos = {"0:20,20", "1:400,20", "2:20,400"};
+// Struktura reprezentująca informacje o małej kulce
+struct Ball {
+    int x;
+    int y;
+};
+
+std::mutex smallBallsMutex;
+std::vector<std::string> playerInfo = {"0:20,20,10", "1:400,20,10", "2:20,400,10"};
+std::vector<Ball> smallBalls;  // Informacje o małych kulach
+
 std::string currentId = "0";
+
+// Funkcja do generowania informacji o małych kulach
+std::string generateSmallBallsInfo() {
+    smallBallsMutex.lock();
+    std::string smallBallsInfo;
+    for (size_t i = 0; i < smallBalls.size(); ++i) {
+        smallBallsInfo += std::to_string(smallBalls[i].x) + "," + std::to_string(smallBalls[i].y);
+        if (i < smallBalls.size() - 1) {
+            smallBallsInfo += "|";
+        }
+    }
+    smallBallsMutex.unlock();
+    return smallBallsInfo;
+}
+
+// Funkcja do inicjalizacji informacji o małych kulach
+void initializeSmallBalls(int numBalls, int boardWidth, int boardHeight) {
+    for (int i = 0; i < numBalls; ++i) {
+        Ball ball;
+        ball.x = rand() % boardWidth;
+        ball.y = rand() % boardHeight;
+        smallBalls.push_back(ball);
+    }
+}
+
+void generateNewBalls() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        // Dodaj nowe małe piłki
+        Ball newBall;
+        newBall.x = rand() % 800;  // Zakładam szerokość planszy 800 (dostosuj do rzeczywistej szerokości)
+        newBall.y = rand() % 600;  // Zakładam wysokość planszy 600 (dostosuj do rzeczywistej wysokości)
+        smallBallsMutex.lock();
+        smallBalls.push_back(newBall);
+        smallBallsMutex.unlock();
+        std::cout << "Added a new small ball at (" << newBall.x << "," << newBall.y << ")" << std::endl;
+    }
+}
 
 void threaded_client(int client_socket) {
     char buffer[2048];
@@ -18,8 +68,11 @@ void threaded_client(int client_socket) {
     currentId = std::to_string(std::stoi(currentId) + 1);
 
     // Send initial positions after sending client ID
-    std::string initialPositions = pos[0] + "|" + pos[1] + "|" + pos[2];
-    send(client_socket, initialPositions.c_str(), initialPositions.size(), 0);
+    std::string initialPlayerPositions = playerInfo[0] + "|" + playerInfo[1] + "|" + playerInfo[2];
+
+    // Send player and smallBalls positions
+    std::string initialMessage = initialPlayerPositions + "?" + generateSmallBallsInfo();
+    send(client_socket, initialMessage.c_str(), initialMessage.size(), 0);
 
     std::string reply;
 
@@ -30,62 +83,94 @@ void threaded_client(int client_socket) {
         if (bytes_received <= 0) {
             send(client_socket, "Goodbye", sizeof("Goodbye"), 0);
             break;
-        } else {
-            std::cout << "Received: " << buffer << std::endl;
+    } else {
+        //std::cout << "Received: " << buffer << std::endl;
 
-            std::string data = buffer;
-            std::string player_data;
-            size_t pos_separator = data.find("|");
+        std::string data = buffer;
+        size_t posSeparator = data.find("?");
 
-            for (int i = 0; i < 3; ++i) {
-                player_data = data.substr(0, pos_separator);
-                data = data.substr(pos_separator + 1);
+        std::string playerData;  // Przeniesienie deklaracji tutaj
 
-                // Extract player ID and position
-                size_t id_separator = player_data.find(":");
-                int player_id = std::stoi(player_data.substr(0, id_separator));
-                std::string player_position = player_data.substr(id_separator + 1);
+        for (int i = 0; i < 3; ++i) {
+            playerData = data.substr(0, posSeparator);
+            data = data.substr(posSeparator + 1);
 
-                // Update the position for the corresponding player ID
-                pos[player_id] = std::to_string(player_id) + ":" + player_position;
+            // Extract player ID, position, and radius
+            size_t idSeparator = playerData.find(":");
+            int playerId = std::stoi(playerData.substr(0, idSeparator));
+            std::string playerPositionAndRadius = playerData.substr(idSeparator + 1);
+
+            // Update the position and radius for the corresponding player ID
+            playerInfo[playerId] = std::to_string(playerId) + ":" + playerPositionAndRadius;
+        }
+
+           // Extract player position and radius after the loop
+            size_t idSeparator = playerData.find(":");
+            int playerId = std::stoi(playerData.substr(0, idSeparator));
+            std::string playerPositionAndRadius = playerData.substr(idSeparator + 1);
+            size_t commaSeparator = playerPositionAndRadius.find(",");
+            int playerX = std::stoi(playerPositionAndRadius.substr(0, commaSeparator));
+            int playerY = std::stoi(playerPositionAndRadius.substr(commaSeparator + 1));
+            size_t lastComma = playerPositionAndRadius.rfind(",");
+            int playerRadius = std::stoi(playerPositionAndRadius.substr(lastComma + 1));
+            smallBallsMutex.lock();
+            for (size_t i = 0; i < smallBalls.size(); ++i) {
+                // Sprawdzamy kolizję z małą piłką
+                int distance = std::sqrt(std::pow(playerX+playerRadius - smallBalls[i].x, 2) + std::pow(playerY+playerRadius - smallBalls[i].y, 2));
+                if (distance < playerRadius) {
+                    // Gracz "zjada" małą piłkę
+                    playerRadius += 2;
+                    // Usuwamy małą piłkę z listy
+                    smallBalls.erase(smallBalls.begin() + i);
+                    std::cout << playerId << ": " << playerRadius << std::endl;
+
+                    // Zaktualizuj playerInfo[playerId] po zwiększeniu promienia
+                    playerInfo[playerId] = std::to_string(playerId) + ":" + std::to_string(playerX) + "," + std::to_string(playerY) + "," + std::to_string(playerRadius);
+                    std::cout << playerInfo[playerId] << std::endl;
+                    break;  // Przerwij pętlę, gdy już doszło do kolizji
+                }
             }
+            smallBallsMutex.unlock();
+            // Construct the reply with the positions and radius of all players
+            reply = playerInfo[0] + "|" + playerInfo[1] + "|" + playerInfo[2];
 
-            // Construct the reply with the positions of all players
-            reply = pos[0] + "|" + pos[1] + "|" + pos[2];
+            // Append small balls positions to the reply
+            reply += "?" + generateSmallBallsInfo();
             std::cout << "Sending: " << reply << std::endl;
         }
 
         send(client_socket, reply.c_str(), reply.size(), 0);
     }
-
     std::cout << "Connection Closed" << std::endl;
     close(client_socket);
 }
 
-
 int main() {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // Initialize smallBalls
+    initializeSmallBalls(20, 800, 600);
+    std::thread newBallsThread(generateNewBalls);
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    sockaddr_in server_address, client_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(5555);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    sockaddr_in serverAddress, clientAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(5555);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
-    listen(server_socket, 2);
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    listen(serverSocket, 2);
     std::cout << "Waiting for a connection" << std::endl;
 
     while (true) {
-        socklen_t client_size = sizeof(client_address);
-        int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_size);
+        socklen_t clientSize = sizeof(clientAddress);
+        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientSize);
 
-        std::cout << "Connected to: " << inet_ntoa(client_address.sin_addr) << std::endl;
+        std::cout << "Connected to: " << inet_ntoa(clientAddress.sin_addr) << std::endl;
 
-        std::thread client_thread(threaded_client, client_socket);
-        client_thread.detach();  // Allow the thread to run independently
+        std::thread clientThread(threaded_client, clientSocket);
+        clientThread.detach();  // Allow the thread to run independently
     }
 
-    close(server_socket);
+    close(serverSocket);
 
     return 0;
 }
